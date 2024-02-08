219bd8c823c79113ee86ede4700120f05d88459d
Kristian Wareing <l1gw@outlook.com>
2024-02-07 17:41:52 -0800

    Updated README

--- README.md
+++ README.md
@@ -1,35 +1,6 @@
 # Reel and Meal
 
-# Description
+This project simulates what it is like in a work evironment to collaborate with fellow team members. With the current knowledge acquired up to this point of the course, we are receiving a movie title and using the OMDB API to determine the genre of the movie. Once the genre is known than an association table is used to determine a food type which is then entered into the Edmam API. The results entail 4 possible recipes that the user can choose to cook a meal with.
 
-This project simulates what it is like in a work evironment to collaborate with fellow team members. With the current knowledge acquired up to this point of the course, we are receiving a movie title and using the OMDB API to determine the genre of the movie. Once the genre is known than an association table is used to determine a food type which is then entered into the Edmam API. The results entail 5 possible recipes that the user can choose to cook a meal with.
-
-# Features
-
-Navigation: Simple and intuitive navigation bar for easy access to different sections of the website.
-Search Form: A search form allows users to enter the title of the movie they want to find a recipe for.
-Search Results: Display of recipes that are recommended for the selected movie. Each recipe is presented in a card format with an image, title, description, and a link to see the full recipe.
-Previous Searches: A section that shows the history of previous user searches, allowing users to easily revisit past searches.
-
-# Technologies Used
-
-HTML
-CSS (Materialize CSS framework)
-JavaScript (with jQuery library for DOM manipulation)
-Material Icons (for navigation icons)
-
-# How to Use
-
-Navigate to https://mylesjav.github.io/Reel-and-Meal/
-Use the search form to enter the title of the movie you want to find a recipe for.
-Click the "Get your Meals" button to search.
-View the recommended recipes in the search results section.
-Optionally, click on "Previous Search" in the navigation bar to see past search history.
-
-# Contributors
-
-Myles, Aileen, Ajay, Kristian
-
-# Website
-
-https://mylesjav.github.io/Reel-and-Meal/
+https://github.com/mylesjav/Reel-and-Meal.git
+https://github.com/mylesjav/Reel-and-Meal
--- script.js
+++ script.js
@@ -1,191 +1,190 @@
 //function to save search terms to local stroage
 function saveSearchToLocal(searchTerm) {
-    //retrieve existing search history from local storage
-    var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-    //add new search term to the search history 
-    searchHistory.push(searchTerm);
-    console.log(searchTerm);
-    //Save the updated search history
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-};
-
-
+  //retrieve existing search history from local storage
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  //add new search term to the search history
+  searchHistory.push(searchTerm);
+  console.log(searchTerm);
+  //Save the updated search history
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+}
 
 //function to display search history
 function displaySearchHistory(targetDivId) {
-    //retrieve search history
-    var searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+  //retrieve search history
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
 
+  //get the search historty div by id
+  var searchHistoryDiv = $("#" + targetDivId);
 
-    //get the search historty div by id
-    var searchHistoryDiv = $('#' + targetDivId);
+  //clear the exisitng content
+  searchHistoryDiv.empty();
 
-    //clear the exisitng content
-    searchHistoryDiv.empty();
-
-    //for loop for each search term to display(jquery)
-    searchHistory.forEach(function (term) {
-        //append a new div to the search history div
-        searchHistoryDiv.append(`<div>${term}</div>`);
-    });
-
-};
-
-
+  //for loop for each search term to display(jquery)
+  searchHistory.forEach(function (term) {
+    //append a new div to the search history div
+    var searchItem = $("<div>").text(term).addClass("previous-search-item");
+    searchHistoryDiv.append(searchItem);
+  });
+}
 
 //enable event handler ready for DOM
 $(document).ready(function () {
+  if (!localStorage.getItem("searchHistory")) {
+    localStorage.setItem("searchHistory", JSON.stringify([]));
+  }
 
-    if (!localStorage.getItem('searchHistory')) {
-        localStorage.setItem('searchHistory', JSON.stringify([]));
+  //show/hide search history button event handler
+  $("#showHistoryBtn").on("click", function () {
+    //toggle the button for search history
+    $("#searchHistory").toggle();
+  });
+
+  displaySearchHistory();
+
+  //event handler for submit button
+  $(".submitBtn").on("click", function (event) {
+    event.preventDefault();
+    console.log("submit button clicked");
+    //setting a variable and setting value for enteredMovie
+    var enteredMovie = $("#icon_prefix").val().trim();
+    //call the function to get movie details
+
+    if (enteredMovie) {
+      logMovie(enteredMovie)
+        .then(function (movieGenre) {
+          //show results container by removing hidden class
+          $(".search-results").removeClass("hidden");
+
+          if (movieGenre && movieGenre.trim() !== "") {
+            $(".actual-results").removeClass("hidden");
+            $(".error-message").addClass("hidden");
+            return getMealsuggestionsByGenre(movieGenre);
+          } else {
+            $(".actual-results").addClass("hidden");
+            $(".error-message").removeClass("hidden");
+            console.error("Genre not found.");
+          }
+        })
+        .then(function () {
+          $(".Name-Tag").text(enteredMovie);
+        })
+        .catch(function (error) {
+          console.error("Error getting movie:", error);
+        });
+    } else {
+      console.error("Please enter a movie.");
     }
+  });
 
-    //show/hide search history button event handler
-    $('#showHistoryBtn').on('click', function () {
-        //toggle the button for search history
-        $("#searchHistory").toggle();
-    });
+  //ombd fetch
 
-    displaySearchHistory();
+  function logMovie(enteredMovie) {
+    var omdbApiKey = "a32eb036";
+    var encodedMovie = encodeURIComponent(enteredMovie);
 
-    //event handler for submit button
-    $('.submitBtn').on('click', function (event) {
-        event.preventDefault();
-        console.log('submit button clicked');
-        //setting a variable and setting value for enteredMovie
-        var enteredMovie = $('#icon_prefix').val().trim();
-        //call the function to get movie details
-        
-        if (enteredMovie) {
-            logMovie(enteredMovie)
-                .then(function (movieGenre) {
-                    if (movieGenre && movieGenre.trim() !== "") {
-                        return getMealsuggestionsByGenre(movieGenre);
-                    } else {
-                        console.error("Genre not found.");
-                    }
-                })
-                .catch(function (error) {
-                    console.error("Error getting movie:", error);
-                });
-        } else {
-            console.error("Please enter a movie.");
-        }
-    });
+    return fetch(
+      `https://www.omdbapi.com/?t=${encodedMovie}&apikey=${omdbApiKey}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (responseJSON) {
+        console.log("Omdb api response:", responseJSON);
+        var movieGenre = responseJSON.Genre;
+        saveSearchToLocal(enteredMovie);
+        displaySearchHistory("previousSearchHistory");
+        return movieGenre;
+      })
+      .catch(function (error) {
+        console.error("Error in Omdb API request:", error);
+        throw error;
+      });
+  }
 
-    //ombd fetch
+  var genreAssociationtable = {
+    action: "Pizza",
+    adventure: "Sandwich",
+    animation: "Colorful",
+    biography: "Hot Dog",
+    comedy: "French fries",
+    crime: "Scary",
+    documentary: "Stew",
+    drama: "Steak",
+    family: "Kid Friendly",
+    fantasy: "English",
+    filmNior: "PotPie",
+    gameShow: "Nachos",
+    history: "Meatloaf",
+    horror: "Scary",
+    music: "Wine",
+    musical: "Wine",
+    mystery: "Spam",
+    news: "Salmon",
+    realityTV: "Salad",
+    romance: "Date Night",
+    sciFi: "Sushi",
+    sport: "Tailgate",
+    talkShow: "Frozen Dinner",
+    thiller: "Italian",
+    war: "Fried Chicken",
+    western: "Western",
+  };
 
+  function getMealsuggestionsByGenre(genre) {
+    var firstGenre = genre
+      .toLowerCase()
+      .split(",")
+      .map((g) => g.trim())[0];
 
-    function logMovie(enteredMovie) {
+    var cuisine = genreAssociationtable[firstGenre];
 
-        var omdbApiKey = "a32eb036";
-        var encodedMovie = encodeURIComponent(enteredMovie);
-
-        return fetch(`https://www.omdbapi.com/?t=${encodedMovie}&apikey=${omdbApiKey}`)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (responseJSON) {
-                console.log("Omdb api response:", responseJSON);
-                var movieGenre = responseJSON.Genre;
-                saveSearchToLocal(enteredMovie);
-                return movieGenre;
-            })
-            .catch(function (error) {
-                console.error("Error in Omdb API request:", error);
-                throw error;
-            });
-
-   
-    };
-
-    var genreAssociationtable = {
-        action: "Pizza",
-        adventure: "Sandwich",
-        animation: "Colorful",
-        biography: "Hot Dog",
-        comedy: "French fries",
-        crime: "Scary",
-        documentary: "Stew",
-        drama: "Steak",
-        family: "Kid Friendly",
-        fantasy: "English",
-        filmNior: "PotPie",
-        gameShow: "Nachos",
-        history: "Meatloaf",
-        horror: "Scary",
-        music: "Wine",
-        musical: "Wine",
-        mystery: "Spam",
-        news: "Salmon",
-        realityTV: "Salad",
-        romance: "Date Night",
-        sciFi: "Sushi",
-        sport: "Tailgate",
-        talkShow: "Frozen Dinner",
-        thiller: "Italian",
-        war: "Fried Chicken",
-        western: "Western",
-    };
-
-
-    function getMealsuggestionsByGenre(genre) {
-
-        var firstGenre = genre.toLowerCase().split(',').map(g => g.trim())[0];
-
-        
-        var cuisine = genreAssociationtable[firstGenre];
-
-        if (cuisine) {
-            console.log("selceted cuisine:", cuisine);
-            logRecipe(cuisine);
-        } else {
-            console.error("No corresponding cuisine found for the movie genre.");
-        }
-
+    if (cuisine) {
+      console.log("selceted cuisine:", cuisine);
+      logRecipe(cuisine);
+    } else {
+      console.error("No corresponding cuisine found for the movie genre.");
     }
+  }
 
-    function logRecipe(cuisine) {
-        var edamamApiKey = "6f74afba579fd22cb4c993e9febf8338";
-        var edamamApiId = "a9029ae2";
+  function logRecipe(cuisine) {
+    var edamamApiKey = "6f74afba579fd22cb4c993e9febf8338";
+    var edamamApiId = "a9029ae2";
 
-        fetch(`https://api.edamam.com/search?q=${cuisine}&app_id=${edamamApiId}&app_key=${edamamApiKey}&to=4`)
-            .then(function (response) {
-                return response.json();
-    
-            })
-            .then(function (responseJSON) {
-                console.log("Edamam Api response:", responseJSON);
+    fetch(
+      `https://api.edamam.com/search?q=${cuisine}&app_id=${edamamApiId}&app_key=${edamamApiKey}&to=4`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (responseJSON) {
+        console.log("Edamam Api response:", responseJSON);
 
-                var mealSuggestions = responseJSON.hits.map(hit => {
-                    return {
-                        recipe: {
-                            label: hit.recipe.label,
-                            image: hit.recipe.image,
-                            source: hit.recipe.source,
-                            url: hit.recipe.url
-                        }
-                    }
-                });
-                displayMealSuggestion(mealSuggestions);
-            })
-            .catch(function (error) {
-                console.error("Error in Edamam api request:", error);
-                throw error;
-            })
-                    
-    }
+        var mealSuggestions = responseJSON.hits.map((hit) => {
+          return {
+            recipe: {
+              label: hit.recipe.label,
+              image: hit.recipe.image,
+              source: hit.recipe.source,
+              url: hit.recipe.url,
+            },
+          };
+        });
+        displayMealSuggestion(mealSuggestions);
+      })
+      .catch(function (error) {
+        console.error("Error in Edamam api request:", error);
+        throw error;
+      });
+  }
 
+  function displayMealSuggestion(mealSuggestions) {
+    var mealSuggestionsContainer = $(".search-results .recipes");
 
-    function displayMealSuggestion(mealSuggestions) {
+    mealSuggestionsContainer.empty();
 
-        var mealSuggestionsContainer = $(".search-results .recipes");
-
-        mealSuggestionsContainer.empty();
-
-        mealSuggestions.forEach(function (meal) {
-
-            mealSuggestionsContainer.append(`
+    mealSuggestions.forEach(function (meal) {
+      mealSuggestionsContainer.append(`
                 <div class="recipe col s12 m3">
                     <div class="card">
                         <div class="card-image">
@@ -201,12 +200,8 @@ $(document).ready(function () {
                     </div>
                 </div>
             `);
-        });
+    });
+  }
 
-    };
-
-
+  displaySearchHistory("previousSearchHistory");
 });
-
-
-
